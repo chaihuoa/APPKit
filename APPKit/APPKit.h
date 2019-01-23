a352569543cc6d4a78454c768002f99e8b36d613
chai <chai.chai@farfetch.com>
2019-01-23 18:29:56 +0800

    init project

---
+++ APPKit/APPKit.h
@@ -0,0 +1,19 @@
+//
+//  APPKit.h
+//  APPKit
+//
+//  Created by chai.chai on 2019/1/23.
+//  Copyright © 2019 chai.chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APPKit.
+FOUNDATION_EXPORT double APPKitVersionNumber;
+
+//! Project version string for APPKit.
+FOUNDATION_EXPORT const unsigned char APPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APPKit/PublicHeader.h>
+
+
