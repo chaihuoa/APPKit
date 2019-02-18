4f4b937c02ff7a8a5b9c901d6bc1d15b02c7c8a3
chai <chai.chai@farfetch.com>
2019-02-18 16:40:37 +0800

    add .gitignore

---
+++ APPKit/Utilities/Array+Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Array+Utils.swift
+//  APPKit
+//
+//  Created by chai.chai on 2019/2/18.
+//  Copyright © 2019 chai.chai. All rights reserved.
+//
+
+import Foundation
---
+++ APPKitDemo/CarouselViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CarouselViewController.swift
+//  APPKitDemo
+//
+//  Created by chai.chai on 2019/2/18.
+//  Copyright © 2019 chai.chai. All rights reserved.
+//
+
+import Foundation
